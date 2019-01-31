83bcd1ff6c3c6160fc86397e0e23cffffb7800ec
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-01-31 17:02:21 -0500

    Assigned this.itemElement to corresponding data attribute

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -3,14 +3,15 @@ class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
     this.element = this.domElement;
-    console.log(element);
+    // console.log(element);
 
     // Get the custom data attribute on the Link
     this.data = element.dataset.tab;
-    console.log(this.data)
+    // console.log(this.data)
     
     // Using the custom data attribute get the associated Item element
-    // this.itemElement;
+    this.itemElement = document.querySelector(`.tabs-item[data-tab = '${element.dataset.tab}']`);
+    console.log(this.itemElement);
     
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
@@ -62,5 +63,5 @@ class TabItem {
 */
 
 const links = document.querySelectorAll('.tabs-link');
-console.log(links);
+// console.log(links);
 links.forEach(items => new TabLink (items));
